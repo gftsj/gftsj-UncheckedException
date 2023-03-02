9dfaba77391418bf692ba405e90e9dbbb0137f6a
Micheal Keeroz <keerozm@gmail.com>
2023-03-02 20:26:19 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,7 +13,19 @@ public class Lab {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
+    
 
+    public void throwUncheckedException(){
+        throw new NullPointerException();
+        
     }
+    // public static void main(String[] args) 
+    // {
+    //   method();
+    // }
+     
+    // public static void method( ) {
+    //   throw new NullPointerException();
+    // }
+    
 }
